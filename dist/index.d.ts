f9dc3e5af9a44a2fa8aa2038e2b831a2701222cd
mailbak36@gmail.com <bachilov@uzkassa.uz>
2021-05-22 23:46:23 +0500

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 import 'antd/dist/antd.css';
+import { RequestFactory, IRequest, ApiProvider, useGet, useGetStream, usePostJSON, usePostJSONStream, usePostFormData, usePostFormDataStream, usePut, usePutStream, useDelete, useDeleteStream } from "./core";
+export { RequestFactory, IRequest, ApiProvider, useGet, useGetStream, usePostJSON, usePostJSONStream, usePostFormData, usePostFormDataStream, usePut, usePutStream, useDelete, useDeleteStream, };
 //# sourceMappingURL=index.d.ts.map
