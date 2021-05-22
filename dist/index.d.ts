3edd02e18ac1dff0455b5badf9b096d99bdb8acb
mailbak36@gmail.com <bachilov@uzkassa.uz>
2021-05-23 01:07:12 +0500

    off react dom injection

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,2 @@
 import 'antd/dist/antd.css';
-import { RequestFactory, IRequest, ApiProvider, useGet, useGetStream, usePostJSON, usePostJSONStream, usePostFormData, usePostFormDataStream, usePut, usePutStream, useDelete, useDeleteStream } from "./core";
-export { RequestFactory, IRequest, ApiProvider, useGet, useGetStream, usePostJSON, usePostJSONStream, usePostFormData, usePostFormDataStream, usePut, usePutStream, useDelete, useDeleteStream, };
 //# sourceMappingURL=index.d.ts.map
